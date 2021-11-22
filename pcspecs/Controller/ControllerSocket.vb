70fbf3eed22cfdfa8db57203f0607204d0ce1bee
Steven Ramirez <steven.ramirez@ucrso.info>
2021-11-22 08:57:02 -0600

    Interfaz de comunicacion meediante sockets lista

---
+++ pcspecs/Controller/ControllerSocket.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Net.Sockets
+Imports System.IO
+Imports System.Net
+
+Public Class ControllerSocket
+
+    Private client As TcpClient
+    Private outputStream As StreamWriter
+
+    'Client possible requests to the server
+    Private SO_COMPLETE_NAME As Byte = 1
+    Private SO_PLATFORM As Byte = 2
+    Private SO_VERSION As Byte = 3
+    Private PC_NAME As Byte = 4
+    Private CPU_INFO As Byte = 5
+    Private TOTAL_RAM As Byte = 6
+    Private DISC_UNITS_LIST As Byte = 7 'Neccesary to show some things
+    Private SCREEN_RESOLUTION As Byte = 8
+    Private LOGGED_USER_NAME As Byte = 9
+    Private SYSTEM_REGION_ZONE As Byte = 10
+    Private DATE_AND_TIME As Byte = 11
+    Private PROCESS_RUNNING_LIST As Byte = 12
+    Private TAKE_SCREESHOT As Byte = 13
+    Private SEND_MESSAGE As Byte = 14
+
+    Public Sub New(hostIp As String, Port As Integer)
+        client = New TcpClient(hostIp, Port)
+        outputStream = New StreamWriter(client.GetStream)
+        outputStream.Flush()
+    End Sub
+
+    Public Sub request(action As Byte)
+        outputStream.Write(action)
+    End Sub
+
+End Class
--- pcspecs/Remote/RemoteForm.vb
+++ pcspecs/Remote/RemoteForm.vb
@@ -1,3 +1,13 @@
-﻿Public Class RemoteForm
+﻿
+Public Class RemoteForm
+
+    Private server As RemoteSocket
+    Private Sub RemoteForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Here the server socket class is instantiated to start with the program
+        server = New RemoteSocket()
+        lb_status.Text = "ESTADO: ESPERANDO POR UNA CONEXIÓN"
+        server.AcceptClient() 'as this is a blocking function the next line is only to update the label
+        lb_status.Text = "ESTADO: CONECTADO A UN EQUIPO CONTROLADOR IPCLIENT: " + server.ShowClientIP()
+    End Sub
 
 End Class
