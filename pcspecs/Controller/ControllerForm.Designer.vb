1748cc258bd7954248298979cdbc1fbf31652e05
Nacho <aegoncold2002@gmail.com>
2021-11-22 11:40:07 -0600

    set the net 5.0 platform

--- pcspecs/Controller/ControllerForm.Designer.vb
+++ pcspecs/Controller/ControllerForm.Designer.vb
@@ -22,17 +22,314 @@ Partial Class ControllerForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(38, 126)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(230, 79)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Nombre Completo SO"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(38, 236)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(230, 77)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Plataforma del SO"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(38, 345)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(230, 79)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Version SO"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(512, 126)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(219, 57)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Nombre del equipo"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(512, 212)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(219, 55)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Informacion del CPU"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(512, 289)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(219, 57)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Total RAM(GB)"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(512, 367)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(219, 57)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Lista Unidades de Disco"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(977, 126)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(208, 48)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Resolucion de pantalla"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(977, 192)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(208, 46)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Nombre de usuario"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(977, 255)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(208, 48)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Zona horaria "
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(977, 324)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(208, 48)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "Fecha y hora del equipo"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(977, 391)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(208, 48)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "Lista de procesos en ejecucion"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(-4, -4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1277, 101)
+        Me.Panel1.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(232, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(835, 51)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Opciones para obtener informacion del equipo remoto"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Location = New System.Drawing.Point(-4, 462)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1277, 82)
+        Me.Panel2.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(232, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(835, 51)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Opciones para controlar equipo remoto"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(38, 596)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(208, 74)
+        Me.Button13.TabIndex = 14
+        Me.Button13.Text = "Realizar captura de pantalla"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(38, 695)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(208, 74)
+        Me.Button14.TabIndex = 15
+        Me.Button14.Text = "Enviar mensaje a equipo remoto"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(339, 596)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(208, 74)
+        Me.Button15.TabIndex = 16
+        Me.Button15.Text = "Subir volumen"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(339, 695)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(208, 74)
+        Me.Button16.TabIndex = 17
+        Me.Button16.Text = "Bajar volumen"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(659, 596)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(208, 74)
+        Me.Button17.TabIndex = 18
+        Me.Button17.Text = "Silenciar sonido"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(659, 695)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(208, 74)
+        Me.Button18.TabIndex = 19
+        Me.Button18.Text = "Apagar el equipo"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(977, 596)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(208, 74)
+        Me.Button19.TabIndex = 20
+        Me.Button19.Text = "Reiniciar equipo"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(977, 695)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(208, 74)
+        Me.Button20.TabIndex = 21
+        Me.Button20.Text = "Cerrar sesion de windows"
+        Me.Button20.UseVisualStyleBackColor = True
         '
         'ControllerForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.ClientSize = New System.Drawing.Size(1269, 805)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "ControllerForm"
         Me.Text = "Controller App"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
 End Class
