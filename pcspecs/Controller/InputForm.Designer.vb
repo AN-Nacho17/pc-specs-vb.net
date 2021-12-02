31254d35b7949f7145b4ecbb979d74f6398e8c4b
Nacho <aegoncold2002@gmail.com>
2021-12-01 23:25:34 -0600

    fixed

--- pcspecs/Controller/InputForm.Designer.vb
+++ pcspecs/Controller/InputForm.Designer.vb
@@ -22,9 +22,45 @@ Partial Class InputForm
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txt_input = New System.Windows.Forms.TextBox()
+        Me.btn_send = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt_input
+        '
+        Me.txt_input.Font = New System.Drawing.Font("JetBrains Mono", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txt_input.Location = New System.Drawing.Point(46, 27)
+        Me.txt_input.Multiline = True
+        Me.txt_input.Name = "txt_input"
+        Me.txt_input.Size = New System.Drawing.Size(728, 347)
+        Me.txt_input.TabIndex = 0
+        '
+        'btn_send
+        '
+        Me.btn_send.BackColor = System.Drawing.Color.Yellow
+        Me.btn_send.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_send.Location = New System.Drawing.Point(341, 413)
+        Me.btn_send.Name = "btn_send"
+        Me.btn_send.Size = New System.Drawing.Size(118, 46)
+        Me.btn_send.TabIndex = 1
+        Me.btn_send.Text = "Enviar"
+        Me.btn_send.UseVisualStyleBackColor = False
+        '
+        'InputForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(810, 471)
+        Me.Controls.Add(Me.btn_send)
+        Me.Controls.Add(Me.txt_input)
+        Me.Name = "InputForm"
+        Me.Text = "Input Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txt_input As TextBox
+    Friend WithEvents btn_send As Button
 End Class
--- pcspecs/Controller/InputForm.vb
+++ pcspecs/Controller/InputForm.vb
@@ -1,6 +1,18 @@
 ﻿Public Class InputForm
 
+    Private UserMessageInput As String
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_send.Click
+        UserMessageInput = txt_input.Text
+    End Sub
 
+    Public Function GetUserInput()
+        Return UserMessageInput
+    End Function
 
+    Private Sub InputForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        UserMessageInput = ""
+        txt_input.Text = ""
+    End Sub
 End Class
